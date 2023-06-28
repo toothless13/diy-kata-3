21fb4981da348e3fd3c65d45eacfbf69e586027e
Tim <50870607+toothless13@users.noreply.github.com>
2023-06-28 16:19:42 +0100

    solve 6th kata

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,17 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+
+    let namesArr = namesObj.map(object => object.name);
+
+    if(namesArr.length === 1) {
+        return namesArr[0];
+    }
+    if(namesArr.length === 2) {
+        return `${namesArr[0]} & ${namesArr[1]}`;
+    }
+    const lastName = namesArr.pop();
+    const secondLastName = namesArr.pop();
+    let namesString = `${namesArr.join(", ")}, ${secondLastName} & ${lastName}`;
+    return namesString;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,56 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+
+  const names1 = [
+    {
+      name: "Marge"
+    },
+    {
+      name: "Bart"
+    },
+    {
+      name: "Lisa"
+    }
+  ];
+
+  const names2 = [
+    {
+      name: "Marge"
+    }
+  ];
+
+  const names3 = [
+    {
+      name: "Marge"
+    },
+    {
+      name: "Bart"
+    }
+  ];
+
+  const names4 = [
+    {
+      name: "Homer"
+    },
+    {
+      name: "Marge"
+    },
+    {
+      name: "Bart"
+    },
+    {
+      name: "Lisa"
+    },
+    {
+      name: "Maggie"
+    }
+  ];
+
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames(names1)).toBe("Marge, Bart & Lisa");
+    expect(joinNames(names2)).toBe("Marge");
+    expect(joinNames(names3)).toBe("Marge & Bart");
+    expect(joinNames(names4)).toBe("Homer, Marge, Bart, Lisa & Maggie");
+  });
 });
