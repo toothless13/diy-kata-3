0b92b13cf64bf82bece8e75af3932e62f190b79d
Tim <50870607+toothless13@users.noreply.github.com>
2023-06-28 12:16:34 +0100

    solve 4th kata

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,37 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+    const yearsArr = [];
+    const catDogFirstYear = 15;
+    const catDogSecondYear = 9;
+    const catDogTwoYears = catDogFirstYear + catDogSecondYear
+    const otherCatYears = 4;
+    const otherDogYears = 5;
+    
+    const catAge = (number) => {
+        return catDogTwoYears + ((number - 2) * otherCatYears);
+    }
+
+    const dogAge = (number) => {
+        return catDogTwoYears + ((number - 2) * otherDogYears);
+    }
+
+    if(number === 0) {
+        return [0, 0, 0];
+    }
+    if(number === 1) {
+        return [1, catDogFirstYear, catDogFirstYear];
+    }
+    if(number === 2) {
+        return [2, catDogTwoYears,catDogTwoYears]
+    }
+    if(number > 2) {
+        yearsArr.push(number);
+
+        yearsArr.push(catAge(number));
+        yearsArr.push(dogAge(number));
+    }
+    return yearsArr;
+
+
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,11 @@
 const { humanCatDogYears } = require("../src");
 
 // Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    it("returns an array of human, cat, and dog years", () => {
+        expect(humanCatDogYears(0)).toStrictEqual([0, 0, 0]);
+        expect(humanCatDogYears(1)).toStrictEqual([1, 15, 15]);
+        expect(humanCatDogYears(2)).toStrictEqual([2, 24, 24]);
+        expect(humanCatDogYears(10)).toStrictEqual([10, 56, 64]);
+    });
+});
