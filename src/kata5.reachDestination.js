b81076303f4d8d2dfdcb3cd06920d4c6b1c6f923
Tim <50870607+toothless13@users.noreply.github.com>
2023-06-28 14:41:48 +0100

    solve 5th kata

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,21 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    if(typeof distance !== 'number') {
+        return false;
+    }
+
+    const initialTime = distance / speed;
+    let timeRounded;
+    const timeRemainder = (initialTime % 1);
+    
+    if(timeRemainder === 0.5 || timeRemainder === 0) {
+        timeRounded = initialTime;
+    } else if(timeRemainder < 0.5) {
+        timeRounded = initialTime + (0.5 - timeRemainder);
+    } else {
+        timeRounded = initialTime + (1 - timeRemainder);
+    }
+
+    return `I should be there in ${timeRounded} hours.`
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,13 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(20, 10)).toBe("I should be there in 2 hours.")
+    expect(reachDestination(35, 6)).toBe("I should be there in 6 hours.")
+    expect(reachDestination(1003, 27)).toBe("I should be there in 37.5 hours.")
+  });
+  it("returns false if arguments are not numbers", () => {
+    expect(reachDestination('bob', false)).toBe(false);
+  });
 });
